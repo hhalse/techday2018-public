9009d8fa5ca98633bff58a4ac92004c26f953460
Patrik Billgren <patrik.billgren@schibsted.com>
2018-12-10 13:53:09 +0100

    Added Image loading task

--- app/src/main/java/no/finn/techday2018/MainActivity.kt
+++ app/src/main/java/no/finn/techday2018/MainActivity.kt
@@ -9,6 +9,7 @@ import no.finn.techday2018.task1.Task1
 import no.finn.techday2018.task2databinding.Task2DataBinding
 import no.finn.techday2018.taskCamera.TaskCamera
 import no.finn.techday2018.taskNotifications.TaskNotifications
+import no.finn.techday2018.taskImage.TaskImage
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,9 +21,11 @@ class MainActivity : AppCompatActivity() {
         taskListAdapter.items = listOf(
             TaskListItem(Task1::class.java, "Hello, world!", R.drawable.hello_world),
             TaskListItem(Task2DataBinding::class.java, "Data binding", R.drawable.eddy),
+            TaskListItem(TaskImage::class.java, "Load Images", R.drawable.hello_world),
             TaskListItem(TaskNotifications::class.java, "Notifications", R.drawable.hwoarang),
             TaskListItem(TaskCamera::class.java, "Take a photo", R.drawable.mokujin),
             TaskListItem(JingleBellsActivity::class.java, getString(R.string.jingle_bells), R.drawable.hello_world)
+
         )
         taskList.adapter = taskListAdapter
         taskList.layoutManager = LinearLayoutManager(this)
