3a2084d48180d1e9ae2b71c2e65acf4d08893314
Håvard Halse <havard.ramundal.halse@finn.no>
2018-12-10 10:53:45 +0100

    Background animation added to data binding

--- app/src/main/java/no/finn/techday2018/task2databinding/Task2DataBinding.kt
+++ app/src/main/java/no/finn/techday2018/task2databinding/Task2DataBinding.kt
@@ -1,9 +1,12 @@
 package no.finn.techday2018.task2databinding
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_task2_databinding.*
 import no.finn.techday2018.R
+import android.graphics.drawable.ColorDrawable
+
 
 class Task2DataBinding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,36 @@ class Task2DataBinding : AppCompatActivity() {
 
     private fun init() {
         editText.onChange { newText -> textView.text = newText}
+
+        //See if you can change the color of the background when clicking the button
+        //And if possible use animation when changing color
+
+
+        button.setOnClickListener { button ->
+            val newColorPair = getNewColors()
+            val animation = ValueAnimator.ofArgb(
+                newColorPair.first,
+                newColorPair.second
+            )
+            animation.addUpdateListener { valueAnimator ->
+                containerView.setBackgroundColor(
+                    valueAnimator.animatedValue as Int
+                )
+            }
+
+            animation.duration = 1000
+            animation.start()
+        }
     }
+
+    private fun getNewColors(): Pair<Int, Int> {
+        val viewColor = containerView.background as ColorDrawable
+        val currentColor = viewColor.color
+        return if (currentColor == resources.getColor(R.color.colorPrimaryDark, theme)) {
+            Pair(resources.getColor(R.color.colorPrimaryDark, theme), resources.getColor(R.color.colorAccent, theme))
+        } else {
+            Pair(resources.getColor(R.color.colorAccent, theme), resources.getColor(R.color.colorPrimaryDark, theme))
+        }
+    }
+
 }
