b887fba38e71de79ddf61724662dd474e639018c
Håvard Halse <havard.ramundal.halse@finn.no>
2018-12-11 15:48:01 +0100

    Styled hello world

--- app/src/main/java/no/finn/techday2018/task1/Task1.kt
+++ app/src/main/java/no/finn/techday2018/task1/Task1.kt
@@ -3,11 +3,13 @@ package no.finn.techday2018.task1
 import no.finn.techday2018.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_task1.*
 
 class Task1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task1)
+        helloWorld.text = "Hello world!"
     }
 }
